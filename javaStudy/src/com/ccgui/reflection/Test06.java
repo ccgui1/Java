4bec13acf917ded7ab80ff82c5d78439fa889bce
“xxr” <1627389570@qq.com>
2022-06-06 17:23:42 +0800

    [ADD] 注解和反射

--- javaStudy/src/com/ccgui/reflection/Test06.java
+++ javaStudy/src/com/ccgui/reflection/Test06.java
@@ -1,25 +1,43 @@
 package com.ccgui.reflection;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.util.PrimitiveIterator;
+import java.lang.reflect.Method;
 
 public class Test06 {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException {
         System.out.println("main");
-//        Class.forName("com.ccgui.reflection.People");
-//        Student student = new Student();
-//        Student.showName();
-//        System.out.println(student.id);
-//          Class c1 = Class.forName("com.ccgui.reflection.Student");
-//         System.out.println(c1.getDeclaredFields());
-//        Student student = new Student();
         Student student = new Student();
         Class  c1 = student.getClass();
+        // 打印Student类的属性
         for( Field field : c1.getFields()){
             System.out.println(field);
         }
-    }
 
+        // 打印类的方法
+        // getMethods()（获取本类以及其父类的全部public方法）
+        // getDeclareMethods() （获取本类的所有方法）
+        Method[] methods = c1.getMethods();
+        for (Method method : methods){
+            System.out.println(method);
+        }
+
+        //获取指定方法
+        // java中的重载，利用参数类型，指明使用的是哪个方法
+        Method getName = c1.getMethod("getName");
+        Method setName = c1.getMethod("setName", String.class);
+
+        // 获取指定的构造器
+        System.out.println("==========构造器============");
+        Constructor[] constructors = c1.getConstructors();
+        for (Constructor constructor : constructors ){
+            System.out.println(constructor);
+        }
+
+        System.out.println(c1.getDeclaredConstructor(String.class,Integer.class));
+
+
+    }
 }
 
 class People{
--- javaStudy/src/com/ccgui/reflection/Test08.java
+++ javaStudy/src/com/ccgui/reflection/Test08.java
@@ -1,4 +1,76 @@
 package com.ccgui.reflection;
+import  com.ccgui.reflection.People;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class Test08 {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+
+        // 获取class对象
+        Class c1 = Class.forName("com.ccgui.reflection.Teacher");
+
+        // 构造一个对象(无参构造器)
+        Teacher  teacher=(Teacher) c1.getDeclaredConstructor().newInstance();
+        System.out.println(teacher);
+
+        // 构造一个对象(有参构造器）
+        Teacher teacher1=(Teacher)c1.getDeclaredConstructor(String.class,String.class).newInstance("张三","学习");
+        System.out.println(teacher1);
+
+        // 通过反射调用普通方法
+        Teacher teacher2 = (Teacher) c1.getDeclaredConstructor().newInstance();
+        Method setName = c1.getDeclaredMethod("setName", String.class);
+        // 注意是是方法的值（setName）
+        // invoke : 激活的意思
+        // (对象，“方法的值”)
+        setName.invoke(teacher2,"憨八龟");
+        System.out.println(teacher2.getName());
+
+        // 通过反射操作属性值
+        Teacher teacher3 = (Teacher) c1.getDeclaredConstructor().newInstance();
+        Field name = c1.getDeclaredField("name");
+
+        // 不能直接操作私有属性，我们需要关闭程序的安全检测
+        // 属性或者方法的setAccessible(true)
+        name.setAccessible(true);
+        name.set(teacher3, "狂神");
+        System.out.println(teacher3.getName());
+    }
+}
+// 创建新类Teacher继承People类
+class Teacher extends People{
+    private String name;
+    private String job;
+
+    // 无参构造器
+    public Teacher(){
+
+    }
+    // 有参构造器
+    public Teacher(String name,String job){
+        this.name = name;
+        this.job = job;
+    }
+    //属性封装
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public  void setJob(String job){
+        this.job = job;
+    }
+    public String getJob(){
+        return this.job;
+    }
+
+    @Override
+    public String toString(){
+        return "Teacher: " + name + " job: " + job;
+    }
+
 }
